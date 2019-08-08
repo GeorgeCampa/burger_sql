2b3bf3ce4ac00351c5389cc48df305a798f0dd79
George <Jc82200172@gmail.com>
2019-08-07 21:44:16 -0700

    inital

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('HamBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheesBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BaconCheeseBurger', false);
