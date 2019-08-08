4fc5202ae30332b2bfa148d10bb50ed10b243e0e
George <49299319+GeorgeCampa@users.noreply.github.com>
2019-08-07 23:17:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 
 
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
